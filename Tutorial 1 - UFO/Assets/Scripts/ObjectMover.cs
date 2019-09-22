4c1ba7656789d5ebf38d7021c3f36f6f673dc392
neoloaf <54559506+neoloaf@users.noreply.github.com>
2019-09-21 20:53:02 -0400

    Challenge 1 - UFO

---
+++ Tutorial 1 - UFO/Assets/Scripts/ObjectMover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMover : MonoBehaviour
+{
+    //transforms as start and end markers (place them as empty objects in the game)
+    public Transform startMarker;
+    public Transform endMarker;
+
+    //movement speed
+    public float speed = 1.0F;
+
+    //time when movement starts
+    private float startTime;
+
+    //total distance between the markers
+    private float journeyLength;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //keep track of when movement started
+        startTime = Time.deltaTime;
+
+        //calculate journey length between start position and end position
+        journeyLength = Vector2.Distance(startMarker.position, endMarker.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //distance moved = time * speed
+        float distCovered = (Time.time - startTime) * speed;
+
+        //fraction of journey completed = current distance divided by the total distance
+        float fractionOfJourney = distCovered / journeyLength;
+
+        //set the position of the object as a fraction of the distance between the two markers, and then ping pong the movement
+        transform.position = Vector2.Lerp(startMarker.position, endMarker.position, Mathf.PingPong(fractionOfJourney, 1));
+    }
+}
--- Tutorial 1 - UFO/Assets/Scripts/PlayerController.cs
+++ Tutorial 1 - UFO/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,25 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    //variables
     public float speed;
     public Text countText;
     public Text winText;
+    public Text livesText;
+    public Text loseText;
     private Rigidbody2D rb2d;
     private int count;
+    private int lives;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D> ();
         count = 0;
+        lives = 3;
         winText.text = "";
+        loseText.text = "";
         SetCountText();
+        SetLivesText();
     }
 
     void Update()
@@ -41,14 +48,45 @@ public class PlayerController : MonoBehaviour
             count = count + 1;
             SetCountText();
         }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
+
+            if (lives == 0)
+            {
+                //stops player from being able to control the ufo
+                Destroy(this);
+                //stops the ufo from moving right there
+                this.gameObject.SetActive(false);
+            }
+        }
+
+        if (count == 12)
+        {
+            //stops the ufo from keeping the momentum when changing levels
+            rb2d.velocity = new Vector2(0.0f, 0.0f);
+            transform.position = new Vector2(0, 70.0f); 
+        }
     }
 
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 12)
+        if (count >= 20)
         {
             winText.text = "You win! Game created by Julianne Truong!";
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+
+        if (lives == 0)
+        {
+           loseText.text = "You lose! :(";
         }
     }
 }
